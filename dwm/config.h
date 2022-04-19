e554eb85ce75ee646c13150b5eaa045db4b58f44
mxhectorvega <mxhectorvega@gmail.com>
2022-04-18 20:12:10 -0500

    ajuste de dependencias lun 18 abr 22 20:12

--- dwm/config.h
+++ dwm/config.h
@@ -12,7 +12,7 @@ static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const char *fonts[]          = { "SF Compact Display:Semibold:size=9", "Font Awesome 5 Free:bold:pixelsize=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "SF Compact Display:Semibold:size=9";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
